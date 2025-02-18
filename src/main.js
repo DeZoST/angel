125fc236e8dd4b79b7c556e7580ddb78af1c9edc
DeZoST <zosted62400@gmail.com>
2025-02-18 04:18:04 +0100

    Working alpha

--- src/main.js
+++ src/main.js
@@ -1,23 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const loader = document.createElement("div");
-    loader.id = "loader";
-    loader.style.position = "fixed";
-    loader.style.top = "0";
-    loader.style.left = "0";
-    loader.style.width = "100%";
-    loader.style.height = "100%";
-    loader.style.background = "white";
-    loader.style.display = "flex";
-    loader.style.justifyContent = "center";
-    loader.style.alignItems = "center";
-    loader.style.fontSize = "1.5rem";
-    loader.innerText = "Chargement...";
-    document.body.appendChild(loader);
-
-    window.addEventListener("load", function () {
-        loader.style.display = "none";
-    });
-
     fetch("/data/content.json")
         .then((response) => response.json())
         .then((data) => {
@@ -31,4 +12,47 @@ document.addEventListener("DOMContentLoaded", function () {
         .catch((error) =>
             console.error("Erreur de chargement du contenu: ", error)
         );
+    const loader = document.querySelector(".loading-screen");
+    const body = document.querySelector("body");
+    loaderFadeOut(loader, body);
+
+    // Intersection Observer for sections
+    const sections = document.querySelectorAll("section");
+    const observerOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 0.5,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.remove("hidden");
+                entry.target.classList.add("show");
+            } else {
+                entry.target.classList.remove("show");
+                entry.target.classList.add("hidden");
+            }
+        });
+    }, observerOptions);
+
+    sections.forEach((section) => {
+        section.classList.add("hidden");
+        observer.observe(section);
+    });
+
+    footer = document.querySelector("footer");
+    footer.classList.add("hidden");
+    observer.observe(footer);
 });
+
+function sleep(ms) {
+    console.log("sleeping for " + ms + "ms");
+    return new Promise((resolve) => setTimeout(resolve, ms));
+}
+async function loaderFadeOut(loader, body) {
+    loader.style.opacity = 0;
+    await sleep(1000);
+    loader.style.display = "none";
+    body.style.overflow = "auto";
+}
